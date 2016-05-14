fff48db0603ed37d3a47c32b906296a0929f1b76
samrat034 <samrat.bhusal@live.com>
2016-05-13 21:25:43 -0500

    Block domain completed

---
+++ src/main/java/mum/edu/dao/BlockDAO.java
@@ -0,0 +1,12 @@
+package mum.edu.dao;
+
+import org.hibernate.SessionFactory;
+
+public class BlockDAO {
+/*SessionFactory sessionFactory;
+	
+	public BlockDAO(SessionFactory sessionFactory){
+		this.sessionFactory =  sessionFactory;
+	}
+*/
+}
