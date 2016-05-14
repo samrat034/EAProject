28ce429774db0c9ec61384d3109154d725f0ec7c
samrat034 <samrat.bhusal@live.com>
2016-05-13 20:34:52 -0500

    Course domain completed

--- src/main/java/mum/edu/domain/Course.java
+++ src/main/java/mum/edu/domain/Course.java
@@ -1,65 +1,143 @@
 package mum.edu.domain;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
-public class Course {
+public class Course implements Serializable {
 	@Id @GeneratedValue
-	private int courseId;
-	private String courseCode;
-	private String courseTitle;
-	private String courseDescriptor;
-	private String courseInstructor;
-	private int courseCapacity;
+	private int id;
+    private String code;
+    private String title;
+    private String description;
+    
+    @OneToMany
+    private List<Course> prerequisites;
+    private String instructor;
+    private int classCapacity;
+    
+    public Course(){
+    	
+    }
+    
+    
+	public Course(int id, String code, String title, String description, List<Course> prerequisites, String instructor,
+			int classCapacity) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.title = title;
+		this.description = description;
+		this.prerequisites = prerequisites;
+		this.instructor = instructor;
+		this.classCapacity = classCapacity;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<Course> getPrerequisites() {
+		return prerequisites;
+	}
+	public void setPrerequisites(List<Course> prerequisites) {
+		this.prerequisites = prerequisites;
+	}
+	public String getInstructor() {
+		return instructor;
+	}
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+	public int getClassCapacity() {
+		return classCapacity;
+	}
+	public void setClassCapacity(int classCapacity) {
+		this.classCapacity = classCapacity;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + classCapacity;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((instructor == null) ? 0 : instructor.hashCode());
+		result = prime * result + ((prerequisites == null) ? 0 : prerequisites.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		if (classCapacity != other.classCapacity)
+			return false;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (instructor == null) {
+			if (other.instructor != null)
+				return false;
+		} else if (!instructor.equals(other.instructor))
+			return false;
+		if (prerequisites == null) {
+			if (other.prerequisites != null)
+				return false;
+		} else if (!prerequisites.equals(other.prerequisites))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Course [code=" + code + "]";
+	}
 	
 	
-	public Course(String courseCode, String courseTitle, String courseDescriptor, Course coursePrerequisite,
-			String courseInstructor, int courseCapacity) {
-		
-		this.courseCode = courseCode;
-		this.courseTitle = courseTitle;
-		this.courseDescriptor = courseDescriptor;
-		this.courseInstructor = courseInstructor;
-		this.courseCapacity = courseCapacity;
-	}
-	public String getCourseCode() {
-		return courseCode;
-	}
-	public void setCourseCode(String courseCode) {
-		this.courseCode = courseCode;
-	}
-	public String getCourseTitle() {
-		return courseTitle;
-	}
-	public void setCourseTitle(String courseTitle) {
-		this.courseTitle = courseTitle;
-	}
-	public String getCourseDescriptor() {
-		return courseDescriptor;
-	}
-	public void setCourseDescriptor(String courseDescriptor) {
-		this.courseDescriptor = courseDescriptor;
-	}
-//	public Course getCoursePrerequisite() {
-//		return coursePrerequisite;
-//	}
-//	public void setCoursePrerequisite(Course coursePrerequisite) {
-//		this.coursePrerequisite = coursePrerequisite;
-//	}
-	public String getCourseInstructor() {
-		return courseInstructor;
-	}
-	public void setCourseInstructor(String courseInstructor) {
-		this.courseInstructor = courseInstructor;
-	}
-	public int getCourseCapacity() {
-		return courseCapacity;
-	}
-	public void setCourseCapacity(int courseCapacity) {
-		this.courseCapacity = courseCapacity;
-	}
+	
+	
+    
+	
 	
 	
 	
