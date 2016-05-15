3d5f0fff7fe35fb99868c60fc08674ec38096124
fikirdebele <fikirdebele@gmail.com>
2016-05-14 18:06:21 -0600

    CourseController

--- src/main/java/mum/edu/controller/CourseController.java
+++ src/main/java/mum/edu/controller/CourseController.java
@@ -1,9 +1,5 @@
 package mum.edu.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import mum.edu.domain.Course;
+import mum.edu.service.CourseService;
 
 @Controller
 @RequestMapping("/course")
